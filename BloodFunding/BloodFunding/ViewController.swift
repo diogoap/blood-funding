876ea12a4ec050afa9b751c8b50af691aa52d50c
Diogo Augusto Pereira <diogo@10-41-48-250.0200.epoa.br.iosda.org>
2019-01-15 16:37:43 -0200

    First commit

---
+++ BloodFunding/BloodFunding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BloodFunding
+//
+//  Created by Diogo Augusto Pereira on 15/01/19.
+//  Copyright © 2019 Diogo Augusto Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
