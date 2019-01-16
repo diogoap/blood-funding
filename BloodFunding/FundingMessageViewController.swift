0d45e8543060b3aeba44e1aa3bd4acbcf0a2c198
Ronaldo Radaieski Cunda <ronaldo@10-41-40-38.0101.epoa.br.iosda.org>
2019-01-16 17:12:19 -0200

    Patrocinar

---
+++ BloodFunding/FundingMessageViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FundingMessageViewController.swift
+//  BloodFunding
+//
+//  Created by Ronaldo Radaieski Cunda on 16/01/19.
+//  Copyright © 2019 Diogo Augusto Pereira. All rights reserved.
+//
+
+import Foundation
